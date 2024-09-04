51dfdbb1a651dec48665c191adca4b37d91dad4c
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-04 19:31:22 +1000

    Added layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import clsx from 'clsx';
+import type { Metadata } from 'next';
+import { Lexend, Source_Code_Pro } from 'next/font/google';
+
+import { Providers } from '@/app/providers';
+import { Layout } from '@/components';
+import '@/styles/globals.css';
+
+const lexend = Lexend({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+const sourceCodePro = Source_Code_Pro({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - RPG Things',
+    default: 'RPG Things',
+  },
+  description: 'A collection of tools for tabletop role-playing games.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={clsx('h-full antialiased', lexend.variable, sourceCodePro.variable)}
+      suppressHydrationWarning
+    >
+      <body className="flex min-h-full bg-white dark:bg-gray-900">
+        <Providers>
+          <Layout>{children}</Layout>
+        </Providers>
+      </body>
+    </html>
+  );
+}

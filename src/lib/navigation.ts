7b6feb96e47a62e234a48412f873da0776b4e666
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-04 19:31:39 +1000

    Added navigation data

---
+++ src/lib/navigation.ts
@@ -0,0 +1,9 @@
+export const navigation = [
+  {
+    title: 'Dice Roller',
+    links: [
+      { title: 'Basic', href: '/dice' },
+      { title: 'With Successes', href: '/dice/success' },
+    ],
+  },
+];

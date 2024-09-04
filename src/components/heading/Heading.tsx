1aca832881c616abe65ab33d9d1bdd09ba4c9f33
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-04 19:37:27 +1000

    Added Heading component

---
+++ src/components/heading/Heading.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx';
+
+type HeadingProps = { level?: 1 | 2 | 3 | 4 | 5 | 6 } & React.ComponentPropsWithoutRef<
+  'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+>;
+
+export function Heading({ className, level = 1, ...props }: HeadingProps) {
+  let Element: `h${typeof level}` = `h${level}`;
+
+  return <Element {...props} className={clsx(className, 'text-gray-900 dark:text-gray-100')} />;
+}
+
+export function Subheading({ className, level = 2, ...props }: HeadingProps) {
+  let Element: `h${typeof level}` = `h${level}`;
+
+  return <Element {...props} className={clsx(className, 'text-gray-900 dark:text-gray-100')} />;
+}

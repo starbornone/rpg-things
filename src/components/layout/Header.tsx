fab128e5aafab6bcee860cda02268b15e056a2bf
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-04 19:37:59 +1000

    Added Header component for layout

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import clsx from 'clsx';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+import { Logo, ThemeSelector } from '@/components';
+
+function GitLabIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg aria-hidden="true" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96zm337.5 12.5l44.6 116.4 .4 1.2c5.6 16.8 7.2 35.2 2.3 52.5c-5 17.2-15.4 32.4-29.8 43.3l-.2 .1-68.4 51.2-54.1 40.9c-.5 .2-1.1 .5-1.7 .8c-2 1-4.4 2-6.7 2c-3 0-6.8-1.8-8.3-2.8l-54.2-40.9L93.5 322.3l-.4-.3-.2-.1c-14.3-10.8-24.8-26-29.7-43.3s-4.2-35.7 2.2-52.5l.5-1.2 44.7-116.4c.9-2.3 2.5-4.3 4.5-5.6c1.6-1 3.4-1.6 5.2-1.8c1.3-.7 2.1-.4 3.4 .1c.6 .2 1.2 .5 2 .7c1 .4 1.6 .9 2.4 1.5c.6 .4 1.2 1 2.1 1.5c1.2 1.4 2.2 3 2.7 4.8l29.2 92.2H285l30.2-92.2c.5-1.8 1.4-3.4 2.6-4.8s2.8-2.4 4.5-3.1c1.7-.6 3.6-.9 5.4-.7s3.6 .8 5.2 1.8c2 1.3 3.7 3.3 4.6 5.6z" />
+    </svg>
+  );
+}
+
+export function Header() {
+  let [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    function onScroll() {
+      setIsScrolled(window.scrollY > 0);
+    }
+    onScroll();
+    window.addEventListener('scroll', onScroll, { passive: true });
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      className={clsx(
+        'sticky top-0 z-10 flex flex-none flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-gray-900/5 transition duration-500 sm:px-6 lg:px-8 dark:shadow-none',
+        isScrolled
+          ? 'dark:bg-gray-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-gray-900/75'
+          : 'dark:bg-transparent'
+      )}
+    >
+      {/* <div className="mr-6 flex lg:hidden">
+        <MobileNavigation />
+      </div> */}
+      <div className="relative flex flex-grow basis-0 items-center">
+        <Link href="/" aria-label="Home page">
+          <Logo className="h-9 w-9 lg:hidden" />
+          <Logo className="hidden h-9 w-auto fill-gray-700 lg:block dark:fill-sky-100" />
+        </Link>
+      </div>
+      <div className="-my-5 mr-6 text-xl font-bold sm:mr-8 md:mr-0">RPG Things</div>
+      <div className="relative z-20 flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
+        <ThemeSelector />
+        <Link href="https://gitlab.com" className="group" aria-label="GitLab">
+          <GitLabIcon className="h-6 w-6 fill-gray-400 group-hover:fill-gray-500 dark:group-hover:fill-gray-300" />
+        </Link>
+      </div>
+    </header>
+  );
+}

59a0ba7849d63f58a1eaf6e20ddf5fe65f56e449
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-04 12:29:52 +1000

    Added setup files

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import tailwindColors from "tailwindcss/colors";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "selector",
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: tailwindColors.zinc,
+      },
+      fontFamily: {
+        sans: ["Lexend", "sans-serif"],
+        mono: ["Source Code Pro", "monospace"],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      maxWidth: {
+        "8xl": "88rem",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+};
+
+export default config;

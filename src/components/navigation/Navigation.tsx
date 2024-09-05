8210c7a8cb66f30c21bb5cccf75f60e60d434268
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-05 10:31:01 +1000

    Simplified Navigation code

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -14,7 +14,23 @@ export function Navigation({
   className?: string;
   onLinkClick?: React.MouseEventHandler<HTMLAnchorElement>;
 }) {
-  let pathname = usePathname();
+  const pathname = usePathname();
+
+  const renderLink = (href: string, title: string, isChildLink?: boolean) => (
+    <Link
+      href={href}
+      onClick={onLinkClick}
+      className={clsx(
+        'block w-full pl-3.5 before:pointer-events-none before:absolute before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full',
+        href === pathname
+          ? 'font-semibold text-cyan-500 before:bg-cyan-500'
+          : 'text-gray-500 before:hidden before:bg-gray-300 hover:text-gray-600 hover:before:block dark:text-gray-400 dark:before:bg-gray-700 dark:hover:text-gray-300',
+        isChildLink ? 'before:-left-1' : 'before:left-0'
+      )}
+    >
+      {title}
+    </Link>
+  );
 
   return (
     <nav className={clsx('text-base lg:text-sm', className)}>
@@ -26,46 +42,23 @@ export function Navigation({
                 {section.title}
               </Heading>
             )}
-            {/* Check if the section has links (array) or a single href */}
-            {section.links ? (
-              <ul
-                role="list"
-                className="mt-2 space-y-2 border-l-2 border-gray-100 lg:mt-4 lg:space-y-4 lg:border-gray-200 dark:border-gray-800"
-              >
-                {section.links.map((link) => (
+            <ul
+              role="list"
+              className={clsx({
+                'mt-2 space-y-2 border-l-2 border-gray-100 lg:mt-4 lg:space-y-4 lg:border-gray-200 dark:border-gray-800':
+                  !!section.links,
+              })}
+            >
+              {section.links ? (
+                section.links.map((link) => (
                   <li key={link.href} className="relative">
-                    <Link
-                      href={link.href}
-                      onClick={onLinkClick}
-                      className={clsx(
-                        'block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full',
-                        link.href === pathname
-                          ? 'font-semibold text-cyan-500 before:bg-cyan-500'
-                          : 'text-gray-500 before:hidden before:bg-gray-300 hover:text-gray-600 hover:before:block dark:text-gray-400 dark:before:bg-gray-700 dark:hover:text-gray-300'
-                      )}
-                    >
-                      {link.title}
-                    </Link>
+                    {renderLink(link.href, link.title, true)}
                   </li>
-                ))}
-              </ul>
-            ) : (
-              /* Render single link if no links array is provided */
-              <div className="relative">
-                <Link
-                  href={section.href}
-                  onClick={onLinkClick}
-                  className={clsx(
-                    'block w-full pl-3.5 before:pointer-events-none before:absolute before:left-0 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full',
-                    section.href === pathname
-                      ? 'font-semibold text-cyan-500 before:bg-cyan-500'
-                      : 'text-gray-500 before:hidden before:bg-gray-300 hover:text-gray-600 hover:before:block dark:text-gray-400 dark:before:bg-gray-700 dark:hover:text-gray-300'
-                  )}
-                >
-                  {section.title}
-                </Link>
-              </div>
-            )}
+                ))
+              ) : (
+                <li className="relative">{renderLink(section.href!, section.title)}</li>
+              )}
+            </ul>
           </li>
         ))}
       </ul>

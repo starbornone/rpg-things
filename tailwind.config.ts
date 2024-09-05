ed59ecae60e6fc6f207e4a6294362a1a0b8e29bd
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-05 11:11:49 +1000

    Added features folder to Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {

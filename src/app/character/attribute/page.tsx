f9d8fca2900d6fb8642902bcb20c1f548f535b0d
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-05 13:31:47 +1000

    Split AttributePage and added Metadata

--- src/app/character/attribute/page.tsx
+++ src/app/character/attribute/page.tsx
@@ -1,61 +1,12 @@
-'use client';
+import { Metadata } from 'next';
+import AttributePage from './AttributePage';
 
-import { Divider, PageHeading, SectionHeading } from '@/components';
-import { selectAttributes, selectSkills } from '@/store/selectors';
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { AttributeForm, SkillForm } from './_forms';
-
-const SkeletonRow = () => (
-  <div className="flex flex-col gap-2">
-    <div className="h-4 w-full rounded bg-gray-100 mt-1"></div>
-    <div className="h-9 w-full rounded bg-gray-100"></div>
-  </div>
-);
-
-const SkeletonLoader = () => (
-  <div className="grid w-full animate-pulse grid-cols-3 gap-4">
-    <SkeletonRow />
-    <SkeletonRow />
-    <SkeletonRow />
-    <SkeletonRow />
-    <SkeletonRow />
-    <SkeletonRow />
-    <SkeletonRow />
-    <SkeletonRow />
-    <SkeletonRow />
-  </div>
-);
+export const metadata: Metadata = {
+  title: 'Attributes & Skills',
+  description:
+    "On this page, you can view and modify your character's core Attributes and Skills, fine-tuning their capabilities to suit the challenges they face. Whether you're building a character focused on physical prowess, social influence, or intellectual mastery, balancing these elements will be key to your success in the game.",
+};
 
 export default function Page() {
-  const attributes = useSelector(selectAttributes);
-  const skills = useSelector(selectSkills);
-
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <div className="flex flex-col gap-8">
-      <PageHeading>Attributes & Skills</PageHeading>
-
-      <div>
-        <SectionHeading>Attributes</SectionHeading>
-        {isLoading ? <SkeletonLoader /> : <AttributeForm attributes={attributes} />}
-      </div>
-
-      <Divider />
-
-      <div>
-        <SectionHeading>Skills</SectionHeading>
-        {isLoading ? <SkeletonLoader /> : <SkillForm skills={skills} />}
-      </div>
-    </div>
-  );
+  return <AttributePage />;
 }

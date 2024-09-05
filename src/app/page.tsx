9f844c35f2ffe08f1624910d2c54cf7660c351bc
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-05 10:38:49 +1000

    Added intro content to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-'use client';
+import { PageHeading } from '@/components';
 
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-
-export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/dice');
-  }, [router]);
-
-  return <div className="flex flex-col gap-8"></div>;
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-8">
+      <PageHeading text="Whether you're a seasoned dungeon master or a new player building your first character, RPG Things provides all the tools you need to enhance your tabletop role-playing experience. From intuitive dice rollers to advanced calculators for attributes and skills, we’ve got you covered. Generate random names, settings, and even entire worlds at the click of a button, giving you the freedom to focus on what matters most: storytelling and strategy. Explore, create, and play – RPG Things is here to help you bring your imagination to life!">
+        Welcome to RPG Things
+      </PageHeading>
+    </div>
+  );
 }

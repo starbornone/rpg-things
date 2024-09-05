f9f3a8f29b94ef2ba9263ac4630825b3c7c44e43
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-05 10:38:40 +1000

    Added basic content to attribute page

---
+++ src/app/attribute/page.tsx
@@ -0,0 +1,9 @@
+import { PageHeading } from '@/components';
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-8">
+      <PageHeading>Basic Attribute Calculator</PageHeading>
+    </div>
+  );
+}

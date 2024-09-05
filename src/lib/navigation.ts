f84748f674c114586787159dd492b36bc2541e99
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-05 10:37:08 +1000

    Added home and basic Attribute pages to Navigation

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -1,9 +1,14 @@
 export const navigation = [
+  { title: 'Introduction', href: '/' },
   {
-    title: 'Dice Roller',
+    title: 'Dice Rollers',
     links: [
       { title: 'Basic', href: '/dice' },
       { title: 'With Successes', href: '/dice/success' },
     ],
+  },
+  {
+    title: 'Attribute Calculator',
+    links: [{ title: 'Basic', href: '/attribute' }],
   },
 ];

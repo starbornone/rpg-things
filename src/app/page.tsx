2fe8762dde982f0f2052861724b9a205f50050a2
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-05 13:32:40 +1000

    Apostrophe annoyance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { PageHeading } from '@/components';
 export default function Page() {
   return (
     <div className="flex flex-col gap-8">
-      <PageHeading text="Whether you're a seasoned dungeon master or a new player building your first character, RPG Things provides all the tools you need to enhance your tabletop role-playing experience. From intuitive dice rollers to advanced calculators for attributes and skills, we’ve got you covered. Generate random names, settings, and even entire worlds at the click of a button, giving you the freedom to focus on what matters most: storytelling and strategy. Explore, create, and play – RPG Things is here to help you bring your imagination to life!">
+      <PageHeading text="Whether you're a seasoned dungeon master or a new player building your first character, RPG Things provides all the tools you need to enhance your tabletop role-playing experience. From intuitive dice rollers to advanced calculators for attributes and skills, we've got you covered. Generate random names, settings, and even entire worlds at the click of a button, giving you the freedom to focus on what matters most: storytelling and strategy. Explore, create, and play – RPG Things is here to help you bring your imagination to life!">
         Welcome to RPG Things
       </PageHeading>
     </div>

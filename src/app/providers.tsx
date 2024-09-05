9bffa1571fa02551b0d5ff84cf30bdea81146eeb
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-05 12:15:57 +1000

    Added redux store to Providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,15 @@
 'use client';
 
+import { store } from '@/store';
 import { ThemeProvider } from 'next-themes';
+import { Provider } from 'react-redux';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" disableTransitionOnChange>
-      {children}
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </Provider>
   );
 }

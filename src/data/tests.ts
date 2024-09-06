e69a724b4bd0b7d3ff406af6606cc144e7ef967c
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-06 19:15:59 +1000

    Added attack type data

---
+++ src/data/tests.ts
@@ -0,0 +1,7 @@
+import { AttackTypeAttributes } from '@/types';
+
+export const attackTypeAttributes: AttackTypeAttributes = {
+  melee: ['strength', 'melee'],
+  firearms: ['dexterity', 'firearms'],
+  brawl: ['strength', 'brawl'],
+};

badc99bc256c550948ffd8daf499983b4b4be6dd
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-07 11:28:39 +1000

    Added some general items

---
+++ src/data/items/other.ts
@@ -0,0 +1,61 @@
+import { Item } from '@/types';
+
+export const healthPotion: Item = {
+  id: 'health-potion',
+  name: 'Health Potion',
+  weight: 0.2,
+  quantity: {
+    current: 1,
+  },
+};
+
+export const rope: Item = {
+  id: 'rope',
+  name: 'Rope',
+  weight: 1.0,
+  durability: {
+    current: 100,
+    max: 100,
+  },
+};
+
+export const lockpicks: Item = {
+  id: 'lockpicks',
+  name: 'Lockpicks',
+  weight: 0.01,
+  quantity: {
+    current: 5,
+  },
+  durability: {
+    current: 5,
+    max: 5,
+  },
+};
+
+export const bandages: Item = {
+  id: 'bandages',
+  name: 'Bandages',
+  weight: 0.01,
+  quantity: {
+    current: 2,
+  },
+};
+
+export const torch: Item = {
+  id: 'torch',
+  name: 'Torch',
+  weight: 0.5,
+  duration: {
+    current: 120, // Remaining burn time in minutes
+    max: 120, // Total burn time in minutes
+  },
+};
+
+export const rations: Item = {
+  id: 'rations',
+  name: 'Rations',
+  weight: 0.5,
+  quantity: {
+    current: 3,
+  },
+};

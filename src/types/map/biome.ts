2a0dad1725d3239e87d13611ea7589fd8ed9c29b
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-07 11:30:14 +1000

    Added biome types

---
+++ src/types/map/biome.ts
@@ -0,0 +1,21 @@
+export type TerrainType =
+  | 'deep water'
+  | 'shallow water'
+  | 'swamp'
+  | 'plains'
+  | 'meadow'
+  | 'savannah'
+  | 'hills'
+  | 'dense forest'
+  | 'light forest'
+  | 'forest clearing'
+  | 'dunes'
+  | 'rocky desert'
+  | 'oasis'
+  | 'canyon'
+  | 'snow plains'
+  | 'glaciers'
+  | 'mountains'
+  | 'frozen river';
+
+export type Biome = 'aquatic' | 'grassland' | 'forest' | 'desert' | 'tundra';

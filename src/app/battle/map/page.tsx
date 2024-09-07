8285f46d5ae056c73563ebec05d403c6e7db4e38
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-07 14:11:33 +1000

    Updated battle map page with intro and to use MapContainer

--- src/app/battle/map/page.tsx
+++ src/app/battle/map/page.tsx
@@ -1,12 +1,14 @@
 import { PageHeading } from '@/components';
-import CanvasGrid from './CanvasGrid';
+import { MapContainer } from './MapContainer';
 
 export default function Page() {
   return (
     <div className="flex flex-col gap-8">
-      <PageHeading>Map Battle Simulator</PageHeading>
+      <PageHeading text="Welcome to the Map Battle Simulator, a dynamic and interactive tool for generating and exploring tactical battle maps. Choose your preferred biome—ranging from aquatic to tundra—and watch as the terrain evolves in real-time, complete with height variations and diverse terrain types. Whether you're strategizing for an intense battle or simply exploring different environments, this simulator lets you visualize various landscapes, from deep waters and rolling plains to towering mountains and dense forests. Dive in and start planning your next move in this immersive tactical world!">
+        Map Battle Simulator
+      </PageHeading>
 
-      <CanvasGrid rows={10} cols={10} unitSize={50} />
+      <MapContainer />
     </div>
   );
 }

e70203957e0e7524eb0357a015be40ba55b7d796
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-07 22:16:45 +1000

    Added defenseTypeAttributes

--- src/data/tests.ts
+++ src/data/tests.ts
@@ -1,7 +1,13 @@
-import { AttackTypeAttributes } from '@/types';
+import { TypeAttributes } from '@/types';
 
-export const attackTypeAttributes: AttackTypeAttributes = {
+export const attackTypeAttributes: TypeAttributes = {
   melee: ['strength', 'melee'],
   firearms: ['dexterity', 'firearms'],
   brawl: ['strength', 'brawl'],
+};
+
+export const defenseTypeAttributes: TypeAttributes = {
+  dodge: ['dexterity', 'athletics'],
+  parry: ['dexterity', 'melee'],
+  block: ['strength', 'brawl'],
 };

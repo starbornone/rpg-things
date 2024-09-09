4599f95d292f4a04c4410a0bc364cc0790c5b196
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-09 12:30:17 +1000

    Updated characterSlice to use templateHuman, added recalculateDerivedValues function, added item related and character create reducers

--- src/store/slices/characterSlice.ts
+++ src/store/slices/characterSlice.ts
@@ -1,88 +1,92 @@
-import { Attributes, Character, Skills } from '@/types';
+import { templateHuman } from '@/data';
+import { Attributes, Character, Item, Skills, Slots, WearableItem } from '@/types';
+import {
+  calculateCarryWeight,
+  calculateCurrentLoad,
+  calculateLift,
+  calculateMaxHp,
+  calculateMove,
+  calculateSpeed,
+  equipWearableItem,
+  updateNestedProperty,
+} from '@/utils';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
 
-const initialState: Character = {
-  id: 'sha001',
-  name: 'Sha',
-  body: {
-    age: 35,
-    gender: 'female',
-    height: 180,
-    species: 'human',
-    weight: 65,
-  },
-  attributes: {
-    strength: 3,
-    dexterity: 3,
-    stamina: 2,
-    charisma: 3,
-    manipulation: 2,
-    composure: 2,
-    intelligence: 2,
-    wits: 2,
-    resolve: 1,
-  },
-  skills: {
-    athletics: 0,
-    brawl: 0,
-    craft: 0,
-    drive: 0,
-    firearms: 0,
-    melee: 0,
-    larceny: 0,
-    stealth: 0,
-    survival: 0,
-    animalKen: 0,
-    etiquette: 0,
-    insight: 0,
-    intimidation: 0,
-    leadership: 0,
-    performance: 0,
-    persuasion: 0,
-    streetwise: 0,
-    subterfuge: 0,
-    academics: 0,
-    awareness: 0,
-    finance: 0,
-    investigation: 0,
-    medicine: 0,
-    occult: 0,
-    politics: 0,
-    science: 0,
-    technology: 0,
-  },
-  health: {
-    currentHp: 100,
-    maxHp: 100,
-  },
-  lift: 5,
-  speed: 5,
-  move: 5,
-  equippedItems: {},
-  heldItems: {},
-  carriedItems: [],
+const recalculateDerivedValues = (state: Character) => {
+  const { strength, stamina, dexterity, wits, resolve } = state.attributes;
+
+  state.carryWeight = calculateCarryWeight(strength, stamina);
+  state.lift = calculateLift(strength, stamina);
+  state.move = calculateMove(dexterity, strength);
+  state.speed = calculateSpeed(dexterity, wits);
+  const maxHP = calculateMaxHp(stamina, resolve);
+  state.health.currentHp = maxHP;
+  state.health.maxHp = maxHP;
+  calculateCurrentLoad(state);
 };
 
-// Create the character slice
 const characterSlice = createSlice({
   name: 'character',
-  initialState,
+  initialState: templateHuman,
   reducers: {
-    // Action to update a specific attribute
-    updateAttribute: (state, action: PayloadAction<{ attribute: keyof Attributes; value: number }>) => {
-      state.attributes[action.payload.attribute] = action.payload.value;
+    // Character-level actions
+    characterCreate: (state, action: PayloadAction<Character>) => {
+      return action.payload;
     },
-    // Action to update a specific skill
-    updateSkill: (state, action: PayloadAction<{ skill: keyof Skills; value: number }>) => {
+    characterUpdate: (state, action: PayloadAction<{ path: string; value: any }>) => {
+      updateNestedProperty(state, action.payload.path, action.payload.value);
+    },
+    characterReset: () => templateHuman,
+
+    // Attribute actions
+    characterAttributesUpdate: (state, action: PayloadAction<{ attribute: keyof Attributes; value: number }>) => {
+      state.attributes[action.payload.attribute] = action.payload.value;
+      recalculateDerivedValues(state);
+    },
+
+    // Skill actions
+    characterSkillsUpdate: (state, action: PayloadAction<{ skill: keyof Skills; value: number }>) => {
       state.skills[action.payload.skill] = action.payload.value;
     },
-    // Action to reset the character to initial state
-    resetCharacter: () => initialState,
+
+    // Equipped items actions
+    characterEquippedItemsAdd: (state, action: PayloadAction<{ item: WearableItem; options?: any }>) => {
+      const newItem = { ...action.payload.item, id: uuidv4() };
+      equipWearableItem(state.equippedItems, newItem, action.payload.options);
+      calculateCurrentLoad(state);
+    },
+    characterEquippedItemsRemove: (state, action: PayloadAction<{ item: WearableItem; slot: Slots }>) => {
+      const { item, slot } = action.payload;
+      state.equippedItems[slot] =
+        state.equippedItems[slot]?.filter((equippedItem) => equippedItem.id !== item.id) || [];
+      state.carriedItems.push(item);
+      calculateCurrentLoad(state);
+    },
+
+    // Carried items actions
+    characterCarriedItemsAdd: (state, action: PayloadAction<Item[]>) => {
+      const itemsWithUniqueIds = action.payload.map((item) => ({ ...item, id: uuidv4() }));
+      state.carriedItems.push(...itemsWithUniqueIds);
+      calculateCurrentLoad(state);
+    },
+    characterCarriedItemsRemove: (state, action: PayloadAction<string>) => {
+      state.carriedItems = state.carriedItems.filter((item) => item.id !== action.payload);
+      calculateCurrentLoad(state);
+    },
   },
 });
 
-// Export the actions
-export const { updateAttribute, updateSkill, resetCharacter } = characterSlice.actions;
+export const {
+  characterCreate,
+  characterUpdate,
+  characterAttributesUpdate,
+  characterSkillsUpdate,
+  characterEquippedItemsAdd,
+  characterCarriedItemsAdd,
+  characterCarriedItemsRemove,
+  characterEquippedItemsRemove,
+  characterReset,
+} = characterSlice.actions;
 
-// Export the reducer to be used in the store
 export default characterSlice.reducer;

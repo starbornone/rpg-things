d00fd361ee1175a438a71d899cc22a97d5e28ba9
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-09 12:29:05 +1000

    Added selectCharacter selector

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,4 +1,5 @@
 import { RootState } from '@/store';
 
+export const selectCharacter = (state: RootState) => state.character;
 export const selectAttributes = (state: RootState) => state.character.attributes;
 export const selectSkills = (state: RootState) => state.character.skills;

63bc592de3f3ab36bb67f9aee80f6f84dd82605c
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-10 11:34:29 +1000

    Added some basic biome resources data

---
+++ src/data/world/biomeResources.ts
@@ -0,0 +1,97 @@
+export const biomeResources = {
+  rainforest: {
+    flora: ['mahogany', 'teak', 'rubber trees', 'bamboo', 'orchids'],
+    fauna: ['exotic bird', 'monkey', 'jaguar', 'frog', 'insect'],
+    minerals: ['bauxite', 'iron ore', 'gold', 'tin', 'copper'],
+  },
+  'wet-forest': {
+    flora: ['cedar', 'oak', 'pine', 'ferns', 'moss'],
+    fauna: ['deer', 'boar', 'wolf', 'bear', 'insect'],
+    minerals: ['limestone', 'clay', 'iron ore', 'quartz', 'granite'],
+  },
+  'moist-forest': {
+    flora: ['softwood timber', 'moss', 'shrubs', 'ferns', 'bushes'],
+    fauna: ['fox', 'rabbit', 'deer', 'owl', 'insect'],
+    minerals: ['granite', 'marble', 'clay', 'coal', 'sandstone'],
+  },
+  'dry-forest': {
+    flora: ['juniper', 'fir', 'sagebrush', 'scrub oak', 'cacti'],
+    fauna: ['boar', 'coyote', 'rattlesnake', 'hawk', 'rodent'],
+    minerals: ['quartz', 'coal', 'limestone', 'iron ore', 'gypsum'],
+  },
+  'very-dry-forest': {
+    flora: ['sagebrush', 'hardwood trees', 'yucca', 'cacti', 'mesquite'],
+    fauna: ['mountain lion', 'bison', 'rattlesnake', 'hawk', 'lizard'],
+    minerals: ['limestone', 'gypsum', 'clay', 'copper', 'coal'],
+  },
+  'thorn-woodland': {
+    flora: ['acacia trees', 'thornbush', 'aloes', 'wildflowers', 'grasses'],
+    fauna: ['antelope', 'giraffe', 'elephant', 'gazelle', 'lion'],
+    minerals: ['copper', 'iron ore', 'gold', 'tin', 'phosphates'],
+  },
+  'desert-scrub': {
+    flora: ['cactus', 'sagebrush', 'yucca', 'succulents', 'mesquite'],
+    fauna: ['snake', 'scorpion', 'lizard', 'jackrabbit', 'vulture'],
+    minerals: ['silver', 'gold', 'uranium', 'copper', 'sulfur'],
+  },
+  desert: {
+    flora: ['succulents', 'desert grasses', 'cacti', 'shrubs', 'wildflowers'],
+    fauna: ['camel', 'fox', 'snake', 'lizard', 'scorpion'],
+    minerals: ['oil', 'natural gas', 'phosphate', 'gypsum', 'salt'],
+  },
+  woodland: {
+    flora: ['oak', 'maple', 'walnut', 'chestnut', 'hazelnut'],
+    fauna: ['deer', 'rabbit', 'squirrel', 'fox', 'hawk'],
+    minerals: ['granite', 'iron ore', 'copper', 'clay', 'limestone'],
+  },
+  steppe: {
+    flora: ['grasses', 'shrubs', 'wildflowers', 'sage', 'thistles'],
+    fauna: ['horse', 'sheep', 'eagle', 'wolf', 'rodent'],
+    minerals: ['salt', 'gypsum', 'coal', 'oil', 'iron ore'],
+  },
+  'rain-tundra': {
+    flora: ['low shrubs', 'grasses', 'mosses', 'ferns', 'lichens'],
+    fauna: ['caribou', 'snow hare', 'wolf', 'bear', 'owl'],
+    minerals: ['coal', 'iron', 'copper', 'gold', 'nickel'],
+  },
+  'wet-tundra': {
+    flora: ['lichen', 'mosses', 'shrubs', 'grasses', 'ferns'],
+    fauna: ['polar bear', 'seal', 'walrus', 'arctic fox', 'seabird'],
+    minerals: ['coal', 'natural gas', 'iron ore', 'oil', 'zinc'],
+  },
+  'moist-tundra': {
+    flora: ['mosses', 'shrubs', 'lichens', 'grasses', 'wildflowers'],
+    fauna: ['reindeer', 'arctic fox', 'wolf', 'hare', 'ptarmigan'],
+    minerals: ['copper', 'zinc', 'iron', 'coal', 'nickel'],
+  },
+  'dry-tundra': {
+    flora: ['lichen', 'low shrubs', 'mosses', 'grasses', 'heath'],
+    fauna: ['wolf', 'arctic hare', 'fox', 'owl', 'lemming'],
+    minerals: ['nickel', 'platinum', 'iron ore', 'silver', 'copper'],
+  },
+  polar: {
+    flora: ['lichen', 'algae', 'mosses', 'snow grasses', 'fungi'],
+    fauna: ['penguin', 'whale', 'seal', 'polar bear', 'krill'],
+    minerals: ['diamonds', 'natural gas', 'oil', 'coal', 'iron ore'],
+  },
+  mountain: {
+    flora: ['coniferous trees', 'alpine flowers', 'mosses', 'shrubs', 'wild grasses'],
+    fauna: ['mountain goat', 'eagle', 'bear', 'snow leopard', 'wolf'],
+    minerals: ['coal', 'silver', 'gold', 'copper', 'iron ore'],
+  },
+  lake: {
+    flora: ['reeds', 'water lilies', 'algae', 'mosses', 'grasses'],
+    fauna: ['fish', 'frog', 'otter', 'duck', 'turtle'],
+    minerals: ['clay', 'limestone', 'sand', 'salt', 'gravel'],
+  },
+  river: {
+    flora: ['willows', 'algae', 'reeds', 'grasses', 'wildflowers'],
+    fauna: ['fish', 'crab', 'beaver', 'water bird', 'otter'],
+    minerals: ['sand', 'silt', 'clay', 'gold', 'gravel'],
+  },
+  ocean: {
+    flora: ['kelp', 'seaweed', 'algae', 'coral', 'mangroves'],
+    fauna: ['fish', 'whale', 'dolphin', 'shark', 'crustacean'],
+    minerals: ['salt', 'oil', 'natural gas', 'phosphates', 'manganese nodules'],
+  },
+};
